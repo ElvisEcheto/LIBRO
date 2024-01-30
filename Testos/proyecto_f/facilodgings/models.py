60505840be6b51d10ebb7ded9ea2af74fa03a1e3
ElvisEcheto <elvisechetob@benjaminherreramedellin.edu.co>
2024-01-29 20:49:35 -0500

    si

--- Testos/proyecto_f/facilodgings/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Facilodgings(models.Model):
-    lodging = models.ForeignKey('lodgings.Lodging', on_delete=models.DO_NOTHING)
-    facilitie = models.ForeignKey('facilities.Facilitie', on_delete=models.DO_NOTHING)
-    status = models.BooleanField(default=True)
-   
-    def __str__(self):
-        return self.lodging
--- Testos/proyecto_f/typedocuments/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [      
-    path('', views.typedocuments, name='typedocuments'),
-    path('typedocument_status_/<int:typedocument_id>/', views.change_status_typedocument, name='typedocument_status'),           
-]
